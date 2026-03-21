ae2640ec650c6f95130d32c731f6038c228a347c
Kevin Andrés Rojas <106268497+kevinroja@users.noreply.github.com>
2026-03-21 17:20:55 +0000

    MiPrimerAngular: Cambios en la sintaxis de Angular para iterar sobre listas

--- MiPrimerAngular/README.md
+++ MiPrimerAngular/README.md
@@ -1,6 +1,6 @@
 # MiPrimerAngular
 
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.1.4.
+This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.2.3.
 
 ## Development server
 
--- MiPrimerAngular/src/app/app.ts
+++ MiPrimerAngular/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ɵJSACTION_BLOCK_ELEMENT_MAP } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('MiPrimerAngular');
+  bienvenidos = 'Hola Belman';
+
+  Estudiante = ['Cuadernos', 'Gorras', 'Gafas']
 }
