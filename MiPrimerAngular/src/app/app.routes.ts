8416730bb737d3a98df7019f7c96afedb123183f
Kevin Andrés Rojas <106268497+kevinroja@users.noreply.github.com>
2026-03-26 02:00:27 +0000

    Creacion de carpetas con neg g component page/inicio y rutas

--- MiPrimerAngular/src/app/app.routes.ts
+++ MiPrimerAngular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {} from './pages/inicio/inicio';
+import {} from './pages/contacto/contacto';
+import {} from './pages/ejercicios/ejercicios';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+];
